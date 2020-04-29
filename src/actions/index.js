11835eb0e85ee85feca06756d78dfb0d9a2c5bfd
小林諒平 <kobayashiryohei@kobayashiryouheinoMacBook-Pro.local>
2020-04-29 20:29:16 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+
+// アクションクリエイター
+export const increment = () => ({type: 'INCREMENT'})
+
+export const decrement = () => ({type: 'DECREMENT'})
